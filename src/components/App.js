b4fef3531579a2e8ed5c0aa6106cc312e16c7390
Juan Diego Jimenez <jdiejim@gmail.com>
2017-10-07 17:20:46 -0600

    Add header links style

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom';
 import Header from './Header';
 import ProjectView from './ProjectView';
 import About from './About';
+import Skills from './Skills';
 import './styles/App.css';
 
 class App extends Component {
@@ -12,6 +13,7 @@ class App extends Component {
         <Header />
         <Route path="/projects" component={ProjectView} />
         <Route path="/about" component={About} />
+        <Route path="/skills" component={Skills} />
       </section>
     );
   }
---
+++ src/components/Skills.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/Skills.css';
+
+function Skills() {
+  return (
+    <section>
+      
+    </section>
+  )
+}
+
+export default Skills;
